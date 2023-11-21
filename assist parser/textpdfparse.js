9aed45a02e872a420cc094418eb095e2c7e34f0e
uno-p-5 <wpasadhika@gmail.com>
2023-11-20 23:46:29 -0800

    Multithreaded PDF processing + fixes

--- assist parser/textpdfparse.js
+++ assist parser/textpdfparse.js
@@ -1,13 +1,15 @@
 import fs from "fs";
+import path from "path"
 import PDFParser from "pdf2json";
+import pLimit from "p-limit"
+import cluster from "cluster"
+import os from "os"
 
-const pdfParser = new PDFParser(undefined, 1);
+// WARNING : any changes to the file will take effect the next time a thread starts, even if the script is already running! 
 
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-pdfParser.on("pdfParser_dataReady", pdfData => {
-    let rawText = pdfParser.getRawTextContent().replaceAll('\u200b', '');
-    // console.log(rawText);
-    // fs.writeFileSync("./output.json", rawText);
+const MAX_WORKER_THREADS = os.cpus().length;
+
+function processPdfData(pdfData, file) {
     //fs.writeFileSync("./debug.json", JSON.stringify(pdfData));
 
     // code is not super great but it also has debugging stuff which is part of it so there's that 
@@ -44,8 +46,34 @@ pdfParser.on("pdfParser_dataReady", pdfData => {
         let currExtraComboMarkerY = 0;
 
         let pageRes = e.Texts.map(t => {
+            let lastWasArrow = false;
             let text = t.R.map(r => {
                 if (r.T == "%E2%80%8B") return ""; // Get rid of zero-width spaces 
+
+                if (r.T == "%E2%86%90") { // left arrow thingy 
+                    addBracket = !braceOpened;
+                    processingLeft = false;
+                    braceOpened = false; 
+                    currArrowY = t.y;
+                    pageArrowYs.push(t.y);
+                    if (lastWasArrow == true) { // Two arrows in a row is impossible, the second one is 100% bad
+                        currLeft = "";
+                    }
+                    lastWasArrow = true;
+
+                    // Check if brace has to be processed 
+                    if (!addBracket) { // This is like doing if braceOpened at the top because we set addBracket to the opposite of braceOpened 
+                        if (prevLeft.trim().length >= 3 && currRight.trim().length >= 3) {
+                            pageArticulations.push([prevLeft, currRight, currArrowY, pageNum])
+                            prevLeft = "";
+                            currRight = "";
+                        }
+                        return "}";
+                    }
+                    return "]←";
+                }
+                if (r.TS.toString() !== [2, 23, 1, 0].toString() && r.TS[1] != 25) return "";
+                lastWasArrow = false;
                 if (r.TS[1] == 25 && t.x > 16 && t.x < 22) {
                     if (currExtraComboMarkerText == "---%20And%20--" || currExtraComboMarkerText == "---%20Or%20--") {
                         pageComboMarkers.push([currExtraComboMarkerText.replaceAll("%20", ' ') + '-', currExtraComboMarkerY])
@@ -57,20 +85,18 @@ pdfParser.on("pdfParser_dataReady", pdfData => {
                     currExtraComboMarkerText += r.T;
                 }
 
-                if (r.T == "%E2%86%90") { // left arrow thingy 
-                    addBracket = true; 
-                    braceOpened = false; 
-                    processingLeft = false;
-                    currArrowY = t.y;
-                    pageArrowYs.push(t.y);
-                    return "]←";
-                }
-                if (r.T == "%3A") {
+
+                if (r.T == "%3A") { // Filter out unrelated text 
                     processingLeft = false;
                     currLeft = "";
                 }
-                if (r.TS.toString() !== [2, 23, 1, 0].toString() && r.TS[1] != 25) return "";
+
+                // Not going to use decodeURIComponent here (at least for now) so we kind of document the other chars and stuff 
                 if (r.T == "%20") return " ";
+                if (r.T == "%26") return "&";
+                if (r.T == "%2B") return "+";
+                if (r.T == "%2F") return "/";
+                if (r.T.length > 3) r.T = r.T.replaceAll("%3A", ":")
     
                 // Check if "[" should be added
                 if (addBracket && t.x < 16) {
@@ -142,7 +168,7 @@ pdfParser.on("pdfParser_dataReady", pdfData => {
     //console.log(arrowYs);
     //console.log(comboMarkers);
 
-    // Post-process articulation ANDs (not doing it in the loop so we can still debug check and stuff) 
+    // Post-process articulation ANDs (not doing it in the loop so we can still debug check and stuff) (we are removing the stuff that the big ANDs handle because realistically nobody's taking multiple courses for a GE)
     for (let i = 0; i < comboMarkers.length; i++) {
         comboMarkers[i].forEach(c => { // Loop through the current page 
             if (c[0] == "--- And ---") {
@@ -165,19 +191,96 @@ pdfParser.on("pdfParser_dataReady", pdfData => {
         });
     }
 
+    // console.log(articulations)
+    
     // Convert articulations to final output form 
     articulations = articulations
         .map(p => p.map(e => [e[0], e[1]])).flat() // Get rid of info we no longer need 
         .filter(e => !e[1].includes("--- And ---")) // Get rid of articulations where you have to take multiple courses because nobody will take those for GEs 
         .map(e => [e[0].replaceAll("--- And ---", ", "), e[1]]) // Convert left side to a list if it counts for both 
         .map(e => e[0].split("--- Or ---").map(c => [c, e[1]])).flat() // If left side has OR then we need to split it into multiple entries 
-        .map(e => e[1].split("--- Or ---").map(c => [e[0], c])).flat() // If right side has OR then we need to split it into multiple entries 
+        .map(e => e[1].split(" | --- Or --- | ").map(c => [e[0], c])).flat() // If right side has OR then we need to split it into multiple entries 
+        .map(e => e[1].split(" | ").map(c => [e[0], c])).flat() // If right side has implicit OR (just listing a shit ton of classes, I'm pretty sure this is OR) we also need to split 
+        .filter(e => e[1].toLowerCase() != "or" && e[1].toLowerCase() != "and"); // Filter out bad edge cases 
+    
+    let jsonObject = {
+        pdfId: file.split('---')[1].split('.pdf')[0],
+        sendingInstitution: file.split('\\')[3].replaceAll('_', ' '),
+        articulations: articulations.map(e => ({to: e[0], from: e[1]}))
+    }
 
-    console.log(articulations);
-
-    // console.log(string)
+    // Print the info 
+    console.log(`Processed ${file.split('\\').slice(3).join('/')}`, jsonObject);
+    //console.log(string)
     //fs.writeFileSync("./debug.txt", string); 
+}
 
-});
+function getAllFilesInDir(directoryPath, filesArray) {
+    let res = filesArray || [];
 
-pdfParser.loadPDF("../scrapers/assist-pdfs/Allan_Hancock_College/Aerospace_Engineering__B_S_---26284155.pdf");
+    const files = fs.readdirSync(directoryPath);
+
+    files.forEach((file) => {
+        const filePath = path.join(directoryPath, file);
+        if (fs.statSync(filePath).isDirectory()) { // If it's a directory we do epic recursive call 
+            getAllFilesInDir(filePath, res);
+        } 
+        else { // If it's a file we just push the path into res 
+            res.push(filePath); 
+        }
+    });
+
+    return res;
+}
+
+async function main() {
+    const allFiles = getAllFilesInDir("../scrapers/assist-pdfs"); // Implement your logic to retrieve files
+    console.log(`Found ${allFiles.length} files!`);
+
+    console.log(`Starting ${Math.min(allFiles.length, MAX_WORKER_THREADS)} threads!`)
+
+    let activeThreads = 0;
+
+    cluster.on('exit', (worker, code, signal) => {
+        //console.log(`Worker ${worker.process.pid} exited with code ${code}`);
+        activeThreads--;
+    });
+
+    for (let i = 0; i < allFiles.length; i++) {
+        // Not crazy efficient because it keeps starting and stopping workers but it's fine :+1:
+        while (activeThreads >= MAX_WORKER_THREADS) await new Promise(t => setTimeout(t, 100));
+        activeThreads++;
+        const worker = cluster.fork();
+        worker.on('message', async (file) => {
+            worker.send(allFiles[i]);
+        });
+    }
+    while (activeThreads > 0) await new Promise(t => setTimeout(t, 100));
+
+} 
+
+if (cluster.isPrimary) {
+    let startTime = Date.now()
+    await main();
+    console.log(`Processing completed in ${(Date.now() - startTime) / 1000} seconds`);
+} 
+else {
+    process.send("a"); // worker is ready 
+    process.on('message', async (file) => {
+        // Process file 
+        const result = await new Promise((resolve, reject) => {
+            const pdfParser = new PDFParser(undefined, 1);
+            pdfParser.on('pdfParser_dataError', errData => {
+                console.log(`ERROR processing ${file.split('\\').slice(3).join('/')}`);
+                console.error(errData.parserError);
+                reject(errData);
+            });
+            pdfParser.on('pdfParser_dataReady', pdfData => {
+                processPdfData(pdfData, file);
+                resolve(pdfData);
+            });
+            pdfParser.loadPDF(file);
+        });
+        process.exit(0);
+    });
+}
