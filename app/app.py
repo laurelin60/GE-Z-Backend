2b7e787737e3add829822fdc72367b3873e1bb20
uno-p-5 <wpasadhika@gmail.com>
2023-11-22 01:06:37 -0800

    ssl support (command line argument)

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from api import create_app
+from sys import argv
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    if ("use_ssl" in argv):
+        app.run(debug=True, host="0.0.0.0",ssl_context=("ssl/certificate.crt", "ssl/private.key"))
+    else:
+        app.run(debug=True, host="0.0.0.0")
