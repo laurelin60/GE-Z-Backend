884cd29e7f08918be5c872f7a8bbf2eba9e15a66
Andrew Wang <awwang3@uci.edu>
2023-12-04 11:34:12 -0800

    refactoring

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from sys import argv
 app = create_app()
 
 if __name__ == '__main__':
-    if ("use_ssl" in argv):
+    if "use_ssl" in argv:
         app.run(debug=True, host="0.0.0.0",ssl_context=("ssl/certificate.crt", "ssl/private.key"))
     else:
         app.run(debug=True, host="0.0.0.0")
